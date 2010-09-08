3b1e1bb79caf893ca209eed01f72cf52fe93a7c9
Malex <malexprojects@gmail.com>
2010-09-08 15:15:16 +0200

    Info finished

--- argopt/info.py
+++ argopt/info.py
@@ -1,14 +1,9 @@
 import re
 
-class INFO:
-	auth_parse = re.compile(r"^\s*([^\s]+)\s+<([^@]+@[^>]+)>\s*$")
+class AUTH:
 
-	def __init__(self,name,version,desc,author,url):
-		self.__n = name
-		self.__v = version
-		self.__d = desc
-		self.__u = url
-		self.__a , self.__m = self._parse(author)
+	def __init__(self,author):
+		self.__n, self.__m = self._parse(author)
 
 	def _parse(self,s):
 		a,m = [],[]
@@ -19,7 +14,46 @@ class INFO:
 				m.append(t.group(2))
 		return " and ".join(a)," or ".join(m)
 
+	def __get__(self):
+		return '\n'.join((' - '.join(a) for a in zip(self.__n.split(" and "), self.__m.split(" or "))))
+
+	@property
+	def name(self):
+		return self.__n
+
+	@property
+	def mail(self):
+		return self.__m
+
+class INFO:
+	auth_parse = re.compile(r"^\s*([^\s]+)\s+<([^@]+@[^>]+)>\s*$")
+
+	def __init__(self,name,version,desc,author,url):
+		self.__n = name
+		self.__v = version
+		self.__d = desc
+		self.__u = url
+		self.__a = AUTH(author)
+
+
 	@property
 	def NAME(self):
 		return self.__n
+
+	@property
+	def VERSION(self):
+		return self.__v
+
+	@property
+	def DESCR(self):
+		return self.__d
+
+	@property
+	def AUTHOR(self):
+		return self.__a
+
+	@property
+	def URL(self):
+		return self.__u
+
 
